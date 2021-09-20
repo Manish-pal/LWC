665a615d4c1ed5badc0ecf598e1efb4cc466e08a
Manish Kumar <kumarmanish2048@gmail.com>
2021-09-21 01:02:40 +0530

    Added Toast message
    Implemented success toast and error toast message.

--- force-app/main/default/lwc/accountManageApex/accountManageApex.js
+++ force-app/main/default/lwc/accountManageApex/accountManageApex.js
@@ -2,6 +2,8 @@ import { LightningElement, track, wire } from 'lwc';
 //importing aura enabled method from @salesforce/apex/<className>/<methodName>
 import getAllAccounts from '@salesforce/apex/accountManager.getAccount';
 
+//for showing the toast message on LWC
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
 export default class AccountManageApex extends LightningElement {
 
@@ -30,9 +32,24 @@ export default class AccountManageApex extends LightningElement {
         getAllAccounts({numOfRecords :this.numberOfRecords}).then(response =>{
 
           this.accounts = response;  
+          const toastEvent = new ShowToastEvent({
+
+            title:'Accounts Loaded',
+            message:this.numberOfRecords + ' Account Fetched from Server',
+            variant:'success'
+          });
+          this.dispatchEvent(toastEvent);
+
         }).catch(error => {
 
             console.log('Error in getting the accounts ', error.body.message);
+            const toastEvent = new ShowToastEvent({
+
+                title:'-- ERROR --',
+                message:error.body.message,
+                variant:'error'
+              });
+              this.dispatchEvent(toastEvent);
         })
 
     }
